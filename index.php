7809bc31707a01194b08dad7a9a9182095d0c739
APasha25 <muhammadalipasha25@gmail.com>
2023-12-17 20:00:50 +0700

    update alif

--- index.php
+++ index.php
@@ -300,10 +300,10 @@
         <div class="col-lg-3 col-md-6 d-flex">
           <i class="bi bi-geo-alt icon"></i>
           <div>
-            <h4>Address</h4>
+            <h4>Alamat</h4>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022 - US<br>
+            Jl. Dipati Ukur No.112-116, Lebakgede <br>
+            Kecamatan Coblong, Kota Bandung, Jawa Barat 40132 <br>
             </p>
           </div>
 
@@ -312,10 +312,10 @@
         <div class="col-lg-3 col-md-6 footer-links d-flex">
           <i class="bi bi-telephone icon"></i>
           <div>
-            <h4>Reservations</h4>
+            <h4>Reservasi</h4>
             <p>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              <strong>Telepon:</strong> 0821-1896-2771<br>
+              <strong>Email:</strong> EduCoffe@gmail.com<br>
             </p>
           </div>
         </div>
@@ -323,16 +323,16 @@
         <div class="col-lg-3 col-md-6 footer-links d-flex">
           <i class="bi bi-clock icon"></i>
           <div>
-            <h4>Opening Hours</h4>
+            <h4>Jam Buka</h4>
             <p>
-              <strong>Mon-Sat: 11AM</strong> - 23PM<br>
-              Sunday: Closed
+              <strong>Senin-Sabtu: 10 Pagi</strong> - 23 Malam<br>
+              Minggu : Tutup
             </p>
           </div>
         </div>
 
         <div class="col-lg-3 col-md-6 footer-links">
-          <h4>Follow Us</h4>
+          <h4>Akun Kami</h4>
           <div class="social-links d-flex">
             <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
             <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
@@ -341,19 +341,6 @@
           </div>
         </div>
 
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Yummy</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
 
