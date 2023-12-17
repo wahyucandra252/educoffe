23eee311151ce7bbe27bf5a2674c167a0e832e46
APasha25 <muhammadalipasha25@gmail.com>
2023-12-17 19:19:54 +0700

    upadate

--- index.php
+++ index.php
@@ -226,8 +226,8 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-header">
-          <h2>Menu - Menu Kami</h2>
-          <p>Cek Menu yang <span>Disajikan di EduCoffe</span></p>
+          <h2>RESEP RESEP KOPI</h2>
+          <p>Macam macam <span>Mininuman kopi</span></p>
         </div>
 
         <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
@@ -244,9 +244,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-1.png" class="glightbox"><img src="assets/img/menu/menu-item-1.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Magnam Tiste</h4>
+                <h4>Cappucino</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                Seperti senandung rasa dalam setiap tegukan, cappucino adalah tarian lembut antara kopi yang kuat dan kelembutan susu
                 </p>
                 <p class="price">
                   $5.95
@@ -255,9 +255,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-2.png" class="glightbox"><img src="assets/img/menu/menu-item-2.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Aut Luia</h4>
+                <h4>Espresso</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                  Dalam kegelapan rasa, espresso adalah cahaya yang memancar, membangunkan indera dengan kekuatan dan keaslian kopi yang penuh karakter.
                 </p>
                 <p class="price">
                   $14.95
@@ -266,9 +266,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-3.png" class="glightbox"><img src="assets/img/menu/menu-item-3.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Est Eligendi</h4>
+                <h4>Black Coffe</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                  Black coffee, sebuah pesta bagi indera; kekuatan kopi murni yang mengalir dalam setiap tegukan, tanpa terhalang oleh tambahan apapun
                 </p>
                 <p class="price">
                   $8.95
@@ -277,9 +277,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-4.png" class="glightbox"><img src="assets/img/menu/menu-item-4.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Eos Luibusdam</h4>
+                <h4>Irish Coffe</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                Dalam sentuhan Irish Coffee, kopi bertemu dengan kehangatan whisky, menciptakan harmoni cita rasa yang memeluk hati dengan lembut.Lorem, deren, trataro, filede, nerada
                 </p>
                 <p class="price">
                   $12.95
@@ -288,9 +288,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-5.png" class="glightbox"><img src="assets/img/menu/menu-item-5.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Eos Luibusdam</h4>
+                <h4>Filter Coffe</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                Dalam secangkir filter coffee, terdapat kelembutan dan kompleksitas cita rasa; seperti mengalami musim kopi yang berubah seiring waktu.
                 </p>
                 <p class="price">
                   $12.95
@@ -299,9 +299,9 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-6.png" class="glightbox"><img src="assets/img/menu/menu-item-6.jpg" class="menu-img img-fluid" alt=""></a>
-                <h4>Laboriosam Direva</h4>
+                <h4>Iced Coffe</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                  Iced coffee, seakan membawa dinginnya embun pagi ke dalam gelas; menyajikan kehangatan kopi dengan kesegaran es yang menggoda.
                 </p>
                 <p class="price">
                   $9.95
