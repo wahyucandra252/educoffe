db9ee875ad05d9e7489dabae2ba2d2fd068d4783
wahyucandra252 <wahyucandrautama@gmail.com>
2023-12-17 20:15:29 +0700

    update wahyu

---
+++ menu/menucapuchino.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>EduCoffe</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Yummy
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="../assets/img/logo.png" alt=""> -->
+        <h1>EduCoffe<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <!-- <li><a href="#hero">Beranda</a></li>
+          <li><a href="#about">Tentang Kami</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#chefs">Paket</a></li> -->
+        </ul>
+      </nav><!-- .navbar -->
+
+      <!-- <a class="btn-book-a-table" href="#book-a-table" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Masuk</a> -->
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+    </section><!-- End Stats Counter Section -->
+
+    <!-- ======= Menu Section ======= -->
+    <section id="menu" class="menu">
+      <div class="container" data-aos="fade-up">
+
+        <!-- <div class="section-header">
+          <h2>RESEP RESEP KOPI</h2>
+          <p>Macam macam <span>Mininuman kopi</span></p>
+        </div> -->
+
+        <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
+
+          <!-- End tab nav item -->
+
+        </ul>
+
+        <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+
+          <div class="tab-pane fade active show" id="menu-starters">
+
+            <div class="row gy-5">
+
+              <div class="col-lg-4 menu-item">
+                <a href="https://youtu.be/8PwqlZJrnuQ?si=_myTRRB1DIF-k7GB" class="glightbox"><img src="../assets/img/menu/menu-item-1.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Cappucino</h4>
+                <p class="ingredients">
+                Seperti senandung rasa dalam setiap tegukan, cappucino adalah tarian lembut antara kopi yang kuat dan kelembutan susu
+                </p>
+                <p class="price">
+                  
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="../assets/img/menu/menu-item-2.png" class="glightbox"><img src="../assets/img/menu/menu-item-2.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Espresso</h4>
+                <p class="ingredients">
+                  Dalam kegelapan rasa, espresso adalah cahaya yang memancar, membangunkan indera dengan kekuatan dan keaslian kopi yang penuh karakter.
+                </p>
+                <p class="price">
+                  
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="../assets/img/menu/menu-item-3.png" class="glightbox"><img src="../assets/img/menu/menu-item-3.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Black Coffe</h4>
+                <p class="ingredients">
+                  Black coffee, sebuah pesta bagi indera; kekuatan kopi murni yang mengalir dalam setiap tegukan, tanpa terhalang oleh tambahan apapun
+                </p>
+                <p class="price">
+                  
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="../assets/img/menu/menu-item-4.png" class="glightbox"><img src="../assets/img/menu/menu-item-4.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Irish Coffe</h4>
+                <p class="ingredients">
+                Dalam sentuhan Irish Coffee, kopi bertemu dengan kehangatan whisky, menciptakan harmoni cita rasa yang memeluk hati dengan lembut.Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="../assets/img/menu/menu-item-5.png" class="glightbox"><img src="../assets/img/menu/menu-item-5.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Filter Coffe</h4>
+                <p class="ingredients">
+                Dalam secangkir filter coffee, terdapat kelembutan dan kompleksitas cita rasa; seperti mengalami musim kopi yang berubah seiring waktu.
+                </p>
+                <p class="price">
+
+                </p>
+              </div><!-- Menu Item -->
+              <div class="col-lg-4 menu-item">
+                <a href="../assets/img/menu/menu-item-6.png" class="glightbox"><img src="../assets/img/menu/menu-item-6.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4>Iced Coffe</h4>
+                <p class="ingredients">
+                  Iced coffee, seakan membawa dinginnya embun pagi ke dalam gelas; menyajikan kehangatan kopi dengan kesegaran es yang menggoda.
+                </p>
+                <p class="price">
+                </p>
+              </div><!-- Menu Item -->
+            </div>
+          </div><!-- End Starter Menu Content -->
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+    <div class="row gy-3">
+        <div class="col-lg-3 col-md-6 d-flex">
+        <i class="bi bi-geo-alt icon"></i>
+        <div>
+            <h4>Alamat</h4>
+            <p>
+            Jl. Dipati Ukur No.112-116, Lebakgede <br>
+            Kecamatan Coblong, Kota Bandung, Jawa Barat 40132 <br>
+            </p>
+        </div>
+
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links d-flex">
+        <i class="bi bi-telephone icon"></i>
+        <div>
+            <h4>Reservasi</h4>
+            <p>
+            <strong>Telepon:</strong> 0821-1896-2771<br>
+            <strong>Email:</strong> EduCoffe@gmail.com<br>
+            </p>
+        </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links d-flex">
+        <i class="bi bi-clock icon"></i>
+        <div>
+            <h4>Jam Buka</h4>
+            <p>
+            <strong>Senin-Sabtu: 10 Pagi</strong> - 23 Malam<br>
+            Minggu : Tutup
+            </p>
+        </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Akun Kami</h4>
+        <div class="social-links d-flex">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div>
+        </div>
+
+    </div>
+    </div>
+
+    </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+<!----- modal ---->
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Masuk</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Masukan Email Anda</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Masukan Password Anda</label>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Masuk</a>
+        <a href="" class="btn btn-danger" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Daftar Akun</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Daftar Akun</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Masukan Nama Lengkap</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Masukan Email</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Masukan No Hp</label>
+        </div>
+        <div class="form-floating mb-3">
+          <select name="" id="" class="form-control">
+            <option value="">-- Pilih Kota --</option>
+            <option value="">BANDUNG</option>
+            <option value="">JAKARTA</option>
+            <option value="">CIAMIS</option>
+            <option value="">BANJAR</option>
+            <option value="">TASIKMALAYA</option>
+          </select>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Buat Password Anda</label>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="" class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal"> Daftar</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
