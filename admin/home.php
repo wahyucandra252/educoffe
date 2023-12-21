1ede5c709749c9766585493cc2b6b4c1179045c9
kemalabduiz <kemalabdulaziz96@gmail.com>
2023-12-21 12:24:02 +0700

    update kemal

--- admin/home.php
+++ admin/home.php
@@ -62,8 +62,6 @@ session_start();
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="../assets32/img/clients/logo_unikom_kuning.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
       <span class="brand-text font-weight-light">EDUCOFFE</span>
     </a>
 
--- admin/sidemenu.php
+++ admin/sidemenu.php
@@ -15,54 +15,25 @@
           </li>
           <li class="nav-item">
             <a href="home.php?menu=8" class="nav-link">
-              <i class="fas fa-tachometer-alt nav-icon"></i>
+              <i class="fas fa-money-check-alt nav-icon"></i>
               <p>Data Pembayaran</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="home.php?menu=8" class="nav-link">
-              <i class="fas fa-tachometer-alt nav-icon"></i>
+              <i class="fas fa-mug-hot nav-icon"></i>
               <p>Data Minuman</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="home.php?menu=8" class="nav-link">
-              <i class="fas fa-tachometer-alt nav-icon"></i>
+              <i class="fas fa-seedling nav-icon"></i>
               <p>Data Biji</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="home.php?menu=8" class="nav-link">
-              <i class="fas fa-tachometer-alt nav-icon"></i>
+              <i class="fas fa-play-circle nav-icon"></i>
               <p>Data Video</p>
             </a>
           </li>
-
-          
-      
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
-              <p>
-                Akun
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="home.php?menu=6" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Profil</p>
-                </a>
-              </li>
-            </ul>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="logout.php" class="nav-link" onclick="return confirm('Anda Yakin Akan Keluar Dari Halaman ini?');">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Logout</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
